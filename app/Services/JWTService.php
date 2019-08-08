fa37140cb2b71143f3b6ae81ff71b95913b75dc0
Hussein El-Attar <hussien.elattar1@gmail.com>
2019-08-08 18:56:51 +0200

    Blacklist access token from refresh token in cache

--- app/Services/JWTService.php
+++ app/Services/JWTService.php
@@ -2,9 +2,14 @@
 
 namespace App\Services;
 
+use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Exceptions\CustomException;
+use Illuminate\Support\Facades\Cache;
 
 class JWTService{
+    const REFRESH_TOKEN_TTL_MINTUES = 60*24; // 24 hours
+    const ACCESS_TOKEN_TTL_MINUTES  = 60; // 1 hour
+
     public function getAccessAndRefreshTokens($credentials)
     {
         $access_token = auth('api')->claims(['type' => 'access_token'])
@@ -15,14 +20,27 @@ class JWTService{
         }
 
         $refresh_token = auth('api')->claims(['type' => 'refresh_token'])
-            ->setTTL(100)->attempt($credentials);
+            ->setTTL(JWTService::REFRESH_TOKEN_TTL_MINTUES)->attempt($credentials);
+
+        Cache::put($refresh_token, $access_token);
 
         return compact('access_token', 'refresh_token');
     }
 
     public function refreshJWT()
     {
-        return JWTAuth::claims(['type' => 'access_token'])
+        $refresh_token    = JWTAuth::getToken();
+        $old_access_token = Cache::get($refresh_token);
+        // blacklist the old token
+        if(!is_null($old_access_token)){
+            JWTAuth::setToken($old_access_token);
+            JWTAuth::invalidate();
+        }
+        $new_access_token = JWTAuth::claims(['type' => 'access_token'])
             ->fromUser(auth('api')->user());
+        // store the newly created (latest) access token
+        Cache::put($refresh_token, $new_access_token, JWTService::ACCESS_TOKEN_TTL_MINUTES * 60);
+
+        return $new_access_token;
     }
 }
